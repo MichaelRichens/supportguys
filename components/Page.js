96b7296b56271cb46ad0469976d207e7b93055a4
Michael Richens <michael.richens@gmail.com>
2021-10-11 19:34:11 +0100

    removed completed todo comment

--- components/Page.js
+++ components/Page.js
@@ -7,7 +7,6 @@ import Footer from "./Footer"
 export default function Page(props) {
 	const canonicalURL = process.env.NEXT_PUBLIC_DOMAIN + useRouter().pathname
 
-	// Example data from MacUpgrades - todo move to .env
 	const structuredDataOrg = {
 		__html: JSON.stringify({
 			"@context": "http://schema.org/",
