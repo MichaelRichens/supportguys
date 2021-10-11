23c7be02e1facc10e5ab897208eea53d55e01fde
Michael Richens <michael.richens@gmail.com>
2021-10-11 21:07:11 +0100

    removed unused import

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { CSSTransition } from "react-transition-group"
 
 import ContactForm from "../components/ContactForm"
