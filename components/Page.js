661e221b82a490e3c1986f763420c164037adde7
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-11 14:48:42 +0100

    added canonical link tag and ability to specify meta description to Page component

--- components/Page.js
+++ components/Page.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Page(props) {
+	const canonicalURL = process.env.NEXT_PUBLIC_DOMAIN + useRouter().pathname
+
 	return (
 		<>
 			<Head>
 				<title>Microsite{props.title && `: ${props.title}`}</title>
+				{props.metaDescription && <meta name="description" content={props.metaDescription} />}
+				<link rel="canonical" href={canonicalURL} />
 			</Head>
 			<Header />
 			<main>
