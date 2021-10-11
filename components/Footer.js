0e522efd08bcbb1d1013a9854ee22018b5ed2cdf
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-11 12:42:10 +0100

    added links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,12 @@
 export default function Footer() {
 	return (
-		<footer>Footer</footer>
+		<footer>
+			Footer
+			<nav>
+				<Link href="/"><a>Home Page</a></Link>
+				<Link href="/aboutus"><a>About Us</a></Link>
+				<Link href="/contactus"><a>Contact Us</a></Link>
+			</nav>
+		</footer>
 	);
 }
