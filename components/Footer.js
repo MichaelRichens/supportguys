f0e0d8439692063bf975f2ec4ccaa8a98034162e
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-11 14:16:59 +0100

    random changes - really to test build is working correctly

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
 	return (
 		<footer>
-			Footer
+			<p>Footer</p>
 			<nav>
 				<Link href="/"><a>Home Page</a></Link>
 				<Link href="/aboutus"><a>About Us</a></Link>
