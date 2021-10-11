2fa551e0d10902b980a1e639160a128113e64196
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-11 13:08:36 +0100

    meaingless change

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Header() {
 	return (
 		<header>
-			Header
+			<p>Header</p>
 			<nav>
 				<Link href="/"><a>Home Page</a></Link>
 				<Link href="/aboutus"><a>About Us</a></Link>
