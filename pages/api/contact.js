902bdacaa2916a1e271cb7d8f151b13e2aa752a4
Michael Richens <michael.richens@gmail.com>
2021-10-12 18:12:26 +0100

    Made contact api return a promise and it seems to work fairly well - kind of cargo cult programming though

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,29 +2,42 @@ import axios from "axios"
 import FormData from "form-data"
 
 //copied from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-async function postData(url = '', data = {}) {
+async function postData(url = "", data = {}) {
 	// Default options are marked with *
 	const response = await fetch(url, {
-	  method: 'POST', // *GET, POST, PUT, DELETE, etc.
-	  mode: 'cors', // no-cors, *cors, same-origin
-	  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-	  credentials: 'same-origin', // include, *same-origin, omit
-	  headers: {
-		'Content-Type': 'application/json'
-		// 'Content-Type': 'application/x-www-form-urlencoded',
-	  },
-	  redirect: 'follow', // manual, *follow, error
-	  referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-	  body: JSON.stringify(data) // body data type must match "Content-Type" header
-	});
-	return response.json(); // parses JSON response into native JavaScript objects
-  }
+		method: "POST", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+			"Content-Type": "application/json"
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		body: JSON.stringify(data) // body data type must match "Content-Type" header
+	})
+	return response.json() // parses JSON response into native JavaScript objects
+}
 
 export default function (req, res) {
-	console.log(req.body);
-	postData('https://www.macupgrades.co.uk/store/fakemail.php', { token: process.env.TOKEN_MAIL, data: req.body })
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
-
-  }
+	return new Promise((resolve, reject) => {
+		postData("https://www.macupgrades.co.uk/store/fakemail.php", {
+			token: process.env.TOKEN_MAIL,
+			data: req.body
+		})
+			.then((data) => {
+				if (data.val == "1") {
+					res.status(200).send()
+					resolve()
+				} else {
+					res.status(500).end()
+					return resolve()
+				}
+			})
+			.catch((error) => {
+				res.status(500).end()
+				return resolve()
+			})
+	})
+}
