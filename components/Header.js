e126c1e550180b0cd10f73ae429fc7d7584d2fea
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-13 15:22:08 +0100

    added links, logo and styles to header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,42 @@
+import Image from "next/image"
 import Link from "next/link"
+
+import logo from "../public/images/support_guys_logo620x100.png"
 
 export default function Header() {
 	return (
 		<header>
-			<p>Header</p>
+			<div class="logo-container">
+				<Link href="/">
+					<Image src={logo} quality="100" />
+				</Link>
+			</div>
 			<nav>
-				<Link href="/"><a>Home Page</a></Link>
-				<Link href="/aboutus"><a>About Us</a></Link>
-				<Link href="/contactus"><a>Contact Us</a></Link>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/contactus">
+					<a>Contact</a>
+				</Link>
+				<Link href="/aboutus">
+					<a>About Us</a>
+				</Link>
+				<Link href="/services">
+					<a>Services</a>
+				</Link>
+				<Link href="/case_studies">
+					<a>Case Studies</a>
+				</Link>
+				<Link href="/pricing">
+					<a>Plans and Costs</a>
+				</Link>
+				<Link href="/support">
+					<a className="urgent">Support Now!</a>
+				</Link>
+				<Link href="/ticket">
+					<a>Tickets</a>
+				</Link>
 			</nav>
 		</header>
-	);
+	)
 }
