5896ee0d10ca60dfd4a0e502ebaff60cc6c81f34
Michael Richens <michael.richens@gmail.com>
2021-10-13 18:50:09 +0100

    fixed class/className

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import logo from "../public/images/support_guys_logo620x100.png"
 export default function Header() {
 	return (
 		<header>
-			<div class="logo-container">
+			<div className="logo-container">
 				<Link href="/">
 					<Image src={logo} quality="100" />
 				</Link>
