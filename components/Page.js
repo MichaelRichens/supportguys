7fbce7ffdb8cd24df828ad3045ded91f7ef75835
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-14 09:39:51 +0100

    fixed attribute

--- components/Page.js
+++ components/Page.js
@@ -85,7 +85,7 @@ export default function Page(props) {
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
-					crossOrigin
+					crossOrigin="true"
 				/>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Spinnaker&display=swap"
