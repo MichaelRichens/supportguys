d986c6e55bc7edd3725f9473809869dfeaf7a5ac
Michael Richens <michael.richens@gmail.com>
2021-10-17 19:29:43 +0100

    started disater recover page, but put on hold - see todo

--- pages/disaster_recovery.js
+++ pages/disaster_recovery.js
@@ -1,3 +1,5 @@
+// TODO get this rewritten in a way that doesn't obviously plagerise the top google result for DRP...
+
 import Page from "../components/Page"
 import ServicePage from "../components/ServicePage"
 
@@ -22,6 +24,14 @@ export default function disaster_recovery() {
 					as your business grows and expands a disaster recovery plan
 					becomes more important.
 				</p>
+				<ul>
+					<li>What if the office did burn down?</li>
+					<li>What if a rogue employee tried to steal your data?</li>
+					<li>What if you had a ransomware attack?</li>
+					<li>What if the office floods?</li>
+					<li>What if your staff can't get to the office?</li>
+					<li>What if all your staff have to work from home?</li>
+				</ul>
 			</ServicePage>
 		</Page>
 	)
