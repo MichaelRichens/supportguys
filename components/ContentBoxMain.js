ee41c051c2ebf53e42fc5f0f62fbfeefe90278b1
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-19 11:02:41 +0100

    adjusted ContentBoxMain to function without any text paragraph at all, just as a title.  Also fixed it so parameters being passed in for margin values actually took effect.

--- components/ContentBoxMain.js
+++ components/ContentBoxMain.js
@@ -2,12 +2,23 @@ import styles from "../styles/ContentBoxMain.module.css"
 
 export default function ContentBoxMain(props) {
 	return (
-		<div className="content-box">
+		<div
+			className={`content-box ${
+				!props.children ? styles["title-only"] : ""
+			}`}
+			style={{
+				marginTop: `${props.marginTop ? props.marginTop : "6em"}`,
+				marginLeft: `${props.marginLeft ? props.marginLeft : "auto"}`,
+				marginRight: `${props.marginRight ? props.marginRight : "auto"}`
+			}}
+		>
 			{props.h1 ? <h1>{props.title}</h1> : <h2>{props.title}</h2>}
-			<div className={styles["text-container-with-line-decorator"]}>
-				<div className={styles["decorator"]}></div>
-				<div className={styles["text"]}>{props.children}</div>
-			</div>
+			{props.children && (
+				<div className={styles["text-container-with-line-decorator"]}>
+					<div className={styles["decorator"]}></div>
+					<div className={styles["text"]}>{props.children}</div>
+				</div>
+			)}
 		</div>
 	)
 }
--- pages/plans_and_costs.js
+++ pages/plans_and_costs.js
@@ -1,6 +1,6 @@
 // TODO think about the background image for this page
 // TODO we talk about how we are different and are up front about costs, then give the costs for non-contracted rates (also need to flag these as more than cotracted rates more)
-// then we spend multiple paragraphs waffling, then our cotracted rates section basically says "it depends"
+// then we spend multiple paragraphs waffling, then our contracted rates section basically says "it depends"
 // I don't think we've justified the first paragraph about being different.
 // Sort out above before continuing
 import Page from "../components/Page"
@@ -20,16 +20,11 @@ export default function plans_and_costs() {
 			>
 				<ContentBoxMain
 					title="Plans and Costs"
-					marginLeft="auto"
-					marginRight="10%"
 					h1={true}
-				>
-					<p>TODO Intro paragraph.</p>
-				</ContentBoxMain>
+				></ContentBoxMain>
 			</section>
 			<section className="standard-text">
 				<h2>Clear Upfront Pricing Information</h2>
-				<h3>TODO - see notes in file about text content</h3>
 			</section>
 		</Page>
 	)
