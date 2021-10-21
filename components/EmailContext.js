96fc3296052846bf5de922ff183a3ecc96e6c7ac
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-21 11:11:09 +0100

    moved more logic into EmailContext to clean up _app.js

--- components/EmailContext.js
+++ components/EmailContext.js
@@ -3,3 +3,64 @@ import { createContext } from "react"
 const EmailContext = createContext()
 
 export default EmailContext
+
+export const initialEmailState = {
+	name: "",
+	nameWarn: "",
+	email: "",
+	emailWarn: "",
+	subject: "",
+	subjectWarn: "",
+	body: "",
+	bodyWarn: "",
+	emailsSent: 0
+}
+
+export function emailReducer(draft, action) {
+	switch (action.type) {
+		case "sent":
+			draft.name = ""
+			draft.nameWarn = ""
+			draft.email = ""
+			draft.emailWarn = ""
+			draft.subject = ""
+			draft.subjectWarn = ""
+			draft.body = ""
+			draft.bodyWarn = ""
+			draft.emailsSent++
+			return
+		case "name":
+			draft.name = action.value
+			return
+		case "nameWarn":
+			draft.nameWarn = action.value
+			return
+		case "email":
+			draft.email = action.value
+			return
+		case "emailWarn":
+			draft.emailWarn = action.value
+			return
+		case "subject":
+			draft.subject = action.value
+			return
+		case "subjectWarn":
+			draft.subjectWarn = action.value
+			return
+		case "body":
+			draft.body = action.value
+			return
+		case "bodyWarn":
+			draft.bodyWarn = action.value
+			return
+		default:
+			if (process.env.NODE_ENV == "development") {
+				console.error(
+					`emailReducer() received unknown action.type: ${action.type}`
+				)
+			} else {
+				console.error("Email form error.")
+			}
+			return
+	}
+}
