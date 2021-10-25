6844d5864c8c17369a6eea31f3586a652af1e9a6
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-25 11:29:26 +0100

    first pass edit of what was the TextOverviewAboutUs component, on index and about us pages

--- pages/about_us.js
+++ pages/about_us.js
@@ -45,12 +45,23 @@ export default function about_us() {
 			<section className="standard-text">
 				<h2>Focus and Specialties</h2>
 				<p>
-					People used to say: "go and speak to those support guys;
-					they will sort it all out for you." And we have, we do, and
-					will continue to do so. We are three guys who have been
-					supporting customers using Apple Mac computers for over
+					Support Guys grew out of{" "}
+					<Link href="https://www.macupgrades.co.uk/store/">
+						MacUpgrades
+					</Link>
+					, which provides Apple Mac repairs and upgrades on a retail
+					basis. Over the years, our relationships with some of our
+					business customers evolved into a more that of a traditional
+					IT support provider, with us handling everything to major
+					network upgrades to printer configuration - "Go and speak to
+					those support guys; they will sort it all out for you." And
+					we have, we do, and will continue to do so.
+				</p>
+				<p>
+					We are a small company that has been supporting customers
+					using Apple Mac computers for over
 					{" " + (new Date().getFullYear() - 2001) + " "} years. We
-					specialise in the world of Apple Mac hardware, macOS and all
+					specialise in the world of Mac hardware, macOS and all
 					associated technologies like WiFi networks, Filemaker
 					servers, NAS solutions, and backups. We can assess, plan and
 					improve your networks and infrastructure and the computers
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-// TODO, actually not very happy with the first sentence...
-
 import ContentBoxMain from "../components/ContentBoxMain"
 import Page from "../components/Page"
 import FindOutMore from "../components/FindOutMore"
@@ -30,14 +28,12 @@ export default function Home() {
 					h1={true}
 				>
 					<p>
-						People used to say: "go and speak to those support guys;
-						they will sort it all out for you." And we have, we do,
-						and will continue to do so. We are three guys who have
-						been supporting customers using Apple Mac computers for
-						over
+						Support Guys provides expert Apple Mac focussed IT
+						support to business customers. We have been supporting
+						customers using Apple Mac computers for over
 						{" " + (new Date().getFullYear() - 2001) + " "} years.
-						We specialise in the world of Apple Mac hardware, macOS
-						and all associated technologies like WiFi networks,
+						We specialise in the world of Mac hardware, macOS and
+						all associated technologies like WiFi networks,
 						Filemaker servers, NAS solutions, and backups. We can
 						assess, plan and improve your networks and
 						infrastructure and the computers you use.
