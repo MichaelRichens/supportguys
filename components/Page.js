b63d0e805bd7f8671dbc15cab50a87d9a0365536
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-26 09:16:59 +0100

    added google analytics tracking

--- components/Page.js
+++ components/Page.js
@@ -120,6 +120,25 @@ export default function Page(props) {
 				>
 					<ContactForm />
 				</PopupOverlay>
+				{/* Global Site Tag (gtag.js) - Google Analytics */}
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				/>
+				{process.env.NODE_ENV == "production" && (
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            					window.dataLayer = window.dataLayer || [];
+            					function gtag(){dataLayer.push(arguments);}
+            					gtag('js', new Date());
+            					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              						page_path: window.location.pathname,
+           				 		});
+          						`
+						}}
+					/>
+				)}
 			</Header>
 			<main>{props.children}</main>
 			<Footer />
