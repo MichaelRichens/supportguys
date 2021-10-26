17bbd6a43b073e9108c3924c1aefc7e17cb43239
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-26 09:45:07 +0100

    added rest of analytics tracking

--- components/Page.js
+++ components/Page.js
@@ -120,15 +120,17 @@ export default function Page(props) {
 				>
 					<ContactForm />
 				</PopupOverlay>
-				{/* Global Site Tag (gtag.js) - Google Analytics */}
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-				/>
+
 				{process.env.NODE_ENV == "production" && (
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
+					<>
+						{/* Global Site Tag (gtag.js) - Google Analytics */}
+						<script
+							async
+							src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+						/>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `
             					window.dataLayer = window.dataLayer || [];
             					function gtag(){dataLayer.push(arguments);}
             					gtag('js', new Date());
@@ -136,8 +138,9 @@ export default function Page(props) {
               						page_path: window.location.pathname,
            				 		});
           						`
-						}}
-					/>
+							}}
+						/>
+					</>
 				)}
 			</Header>
 			<main>{props.children}</main>
