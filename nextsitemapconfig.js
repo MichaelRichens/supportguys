4ee80bec80bbd9c04e8e1e067eaa078b3b9f457c
Michael Richens <michael@2ndchancepc.co.uk>
2021-10-29 16:39:52 +0100

    added next/sitemap

---
+++ nextsitemapconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+	siteUrl: "https://www.supportguys.co.uk",
+	changefreq: "weekly",
+	priority: 0.7,
+	generateRobotsTxt: true,
+	// Default transformation function
+	transform: async (config, path) => {
+		return {
+			loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+			changefreq: config.changefreq,
+			priority: config.priority,
+			lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+			alternateRefs: config.alternateRefs ?? []
+		}
+	},
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: "*",
+				allow: "/"
+			}
+		]
+	}
+}
